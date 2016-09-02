2abbb44a191b12ee33d8880d1556ab9241d695b1
Chia Huang <chialh@uci.edu>
2016-09-02 14:18:46 -0500

    Commented out Test

--- Tests/SwiftCalendarTests/MainTests.swift
+++ Tests/SwiftCalendarTests/MainTests.swift
@@ -1,39 +1,39 @@
-#if os(Linux)
-    import Glibc
-#else
-    import Darwin
-#endif
-
-import XCTest
-import Foundation
-
-@testable import SwiftCalendar
-
-class MainTests: XCTestCase {
-    
-    static var allTests: [(String, (MainTests) -> () throws -> Void)] {
-        return [
-            ("testCalendarInitializerWithISO8601", testCalendarInitializerWithISO8601),
-            ("testCalendarInitializerWithBuddhist", testCalendarInitializerWithBuddhist),
-            ("testCalendarInitializerWithIndian", testCalendarInitializerWithIndian),
-            ("testCalendarInitializerWithJapanese", testCalendarInitializerWithJapanese)
-        ]
-    }
-    
-    func testCalendarInitializerWithISO8601() {
-        testCalendarWithIdentifierISO8601()
-    }
-    
-    func testCalendarInitializerWithBuddhist() {
-        testCalendarWithIdentifierBuddhist()
-    }
-    
-    func testCalendarInitializerWithIndian() {
-        testCalendarWithIdentifierIndian()
-    }
-    
-    func testCalendarInitializerWithJapanese() {
-        testCalendarWithIdentifierJapanese()
-    }
-    
-}
+//#if os(Linux)
+//    import Glibc
+//#else
+//    import Darwin
+//#endif
+//
+//import XCTest
+//import Foundation
+//
+//@testable import SwiftCalendar
+//
+//class MainTests: XCTestCase {
+//    
+//    static var allTests: [(String, (MainTests) -> () throws -> Void)] {
+//        return [
+//            ("testCalendarInitializerWithISO8601", testCalendarInitializerWithISO8601),
+//            ("testCalendarInitializerWithBuddhist", testCalendarInitializerWithBuddhist),
+//            ("testCalendarInitializerWithIndian", testCalendarInitializerWithIndian),
+//            ("testCalendarInitializerWithJapanese", testCalendarInitializerWithJapanese)
+//        ]
+//    }
+//    
+//    func testCalendarInitializerWithISO8601() {
+//        testCalendarWithIdentifierISO8601()
+//    }
+//    
+//    func testCalendarInitializerWithBuddhist() {
+//        testCalendarWithIdentifierBuddhist()
+//    }
+//    
+//    func testCalendarInitializerWithIndian() {
+//        testCalendarWithIdentifierIndian()
+//    }
+//    
+//    func testCalendarInitializerWithJapanese() {
+//        testCalendarWithIdentifierJapanese()
+//    }
+//    
+//}
